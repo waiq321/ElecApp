804899c6b70bcabc6781c403ecc7193be091d299
Waqas <waiq321@gmail.com>
2018-01-12 00:04:27 +0500

    Layout Change

---
+++ App_Code/DBManager.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+/// <summary>
+/// Summary description for DBManager
+/// </summary>
+public class DBManager
+{
+    SqlDataAdapter _SqlAdapter = new SqlDataAdapter();
+    SqlConnection _SqlCon;
+    SqlCommand _SqlCommand = new SqlCommand();
+    DataTable _dataTable = new DataTable();
+    DataSet _dataSet = new DataSet();
+    public void Openconn(string connStr)
+    {
+        try
+        {
+            string constr = ConfigurationManager.ConnectionStrings[connStr].ConnectionString;
+            _SqlCon = new SqlConnection(constr);
+            _SqlCon.Open();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public DataTable ExecuteDataTable(string ProcedureName, string connStr, SqlParameter[] prms)
+    {
+        try
+        {
+            Openconn(connStr);
+            _SqlCommand = new SqlCommand(ProcedureName, _SqlCon);
+            _SqlCommand.CommandType = CommandType.StoredProcedure;
+            _SqlCommand.CommandText = ProcedureName;
+            foreach (SqlParameter sqlParam in prms)
+            {
+                _SqlCommand.Parameters.Add(sqlParam);
+            }
+
+            _SqlAdapter.SelectCommand = _SqlCommand;
+            _SqlAdapter.Fill(_dataTable);
+            _SqlCon.Close();
+            _SqlCommand.Dispose();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return _dataTable;
+    }
+
+    public DataTable ExecuteDataTableWithQuery(string query, string connStr, SqlParameter[] prms)
+    {
+        try
+        {
+            Openconn(connStr);
+            _SqlCommand = new SqlCommand(query, _SqlCon);
+            _SqlCommand.CommandType = CommandType.Text;
+            foreach (SqlParameter sqlParam in prms)
+            {
+                _SqlCommand.Parameters.Add(sqlParam);
+            }
+
+            _SqlAdapter.SelectCommand = _SqlCommand;
+            _SqlAdapter.Fill(_dataTable);
+            _SqlCon.Close();
+            _SqlCommand.Dispose();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return _dataTable;
+    }
+
+    public DataSet ExecuteDataSet(string ProcedureName, string connStr, SqlParameter[] prms)
+    {
+        try
+        {
+            Openconn(connStr);
+            _SqlCommand = new SqlCommand(ProcedureName, _SqlCon);
+            _SqlCommand.CommandType = CommandType.StoredProcedure;
+            _SqlCommand.CommandText = ProcedureName;
+            foreach (SqlParameter sqlParam in prms)
+            {
+                _SqlCommand.Parameters.Add(sqlParam);
+            }
+
+            _SqlAdapter.SelectCommand = _SqlCommand;
+            _SqlAdapter.Fill(_dataSet);
+            _SqlCon.Close();
+            _SqlCommand.Dispose();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return _dataSet;
+    }
+
+    public void ExecuteNonQuery(string ProcedureName, string connStr, SqlParameter[] prms)
+    {
+        try
+        {
+            Openconn(connStr);
+            _SqlCommand = new SqlCommand(ProcedureName, _SqlCon);
+            _SqlCommand.CommandType = CommandType.StoredProcedure;
+            _SqlCommand.CommandText = ProcedureName;
+            foreach (SqlParameter sqlParam in prms)
+            {
+                _SqlCommand.Parameters.Add(sqlParam);
+            }
+
+            _SqlCommand.ExecuteNonQuery();
+            _SqlCon.Close();
+            _SqlCommand.Dispose();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+
+    public DataTable ExecuteDataTable_Query(string query, string connStr, SqlParameter[] prms)
+    {
+        try
+        {
+            Openconn(connStr);
+            _SqlCommand = new SqlCommand(query, _SqlCon);
+            foreach (SqlParameter sqlParam in prms)
+            {
+                _SqlCommand.Parameters.Add(sqlParam);
+            }
+
+            _SqlAdapter.SelectCommand = _SqlCommand;
+            _SqlAdapter.Fill(_dataTable);
+            _SqlCon.Close();
+            _SqlCommand.Dispose();
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return _dataTable;
+    }
+}
---
+++ Candidates/AddCandidate.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMaster.master" AutoEventWireup="true" CodeFile="AddCandidate.aspx.cs" Inherits="Candidates_AddCandidate" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table>
+        <tr>
+            <td>Name:</td>
+            <td>
+                <input type="text"  id="txtName"/>
+            </td>            
+            <td colspan="2" rowspan="3">
+                <asp:Image ID="picture" runat="server" />
+            </td>
+        </tr>
+        <tr>
+            <td>NIC#:</td>
+            <td><input type="text"  id="txtNIC"/></td>            
+        </tr>
+        <tr>
+            <td>Party:</td>
+            <td>
+                <select id="ddlParty"></select>
+            </td>   
+            <td>Relation With Judiciary:</td>
+            <td></td>
+        </tr>
+
+    </table>
+</asp:Content>
+
+
